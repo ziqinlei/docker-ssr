7aa23a404322787735d210d9aecabe2ca8237e11
ziqin.lei <ziqin.lei@yingzt.com>
2018-06-26 13:58:56 +0800

    ADD docker_run.sh

---
+++ docker_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build \
+	-t hub.docker.com/ziqinlei/shadowsorcksr:1.0 \
+	-t hub.docker.com/ziqinlei/shadowsorcksr:latest \
+	.
---
+++ docker_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d --name shadowsocksr -p 2333:2333
